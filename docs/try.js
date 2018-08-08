92bf93e284b62602c15d224f129f7c7a5441244e
irori <irorin@gmail.com>
2018-08-08 20:39:10 +0900

    Tameshigaki

---
+++ docs/try.js
@@ -0,0 +1,22 @@
+if (document.fonts) {
+    let text = document.getElementById('try-text');
+    let size = document.getElementById('try-size');
+    let display = document.getElementById('try-display');
+    let face;
+
+    document.getElementById('try').hidden = false;
+    text.addEventListener('focus', () => {
+        if (!face) {
+            face = new FontFace('ワープロ明朝full', 'url("wapuro-mincho.woff2") format("woff2")');
+            document.fonts.add(face);
+            face.load();
+        }
+    });
+    text.addEventListener('input', () => {
+        display.textContent = text.value;
+    });
+    size.addEventListener('change', () => {
+        display.style.fontSize = size.value;
+    });
+    display.style.fontSize = size.value;
+}
