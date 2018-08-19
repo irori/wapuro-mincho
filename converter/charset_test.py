b675665f5558cae02f05a7fd466e8985d2050f71
irori <irorin@gmail.com>
2018-08-19 20:52:28 +0900

    Add charset_test.py

---
+++ converter/charset_test.py
@@ -0,0 +1,26 @@
+import unittest
+
+from bdflib import reader
+import charset
+
+
+class JISTest(unittest.TestCase):
+
+    def test_jis(self):
+        with open('bdf/jiskan24-2003-1.bdf') as f:
+            bdf = reader.read_bdf(f)
+        cconv = charset.JIS()
+
+        single_cp = 0
+        multi_cp = 0
+        for cp in bdf.codepoints():
+            unicode = cconv.unicode(cp)
+            if unicode is None:
+                pass
+            elif len(unicode) == 1:
+                single_cp += 1
+            else:
+                multi_cp += 1
+
+        self.assertEqual(single_cp, 8772)
+        self.assertEqual(multi_cp, 25)
